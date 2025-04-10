222617d12ff3e86474b8b5dc962b51a1680280f0
nrturner1980 <nrturner99@gmail.com>
2025-04-10 00:46:43 +0000

    doing python math

---
+++ welcome.py
@@ -0,0 +1,17 @@
+line01 = "***********************"
+line02 = "*                     *"
+line03 = "*       WELCOME!      *"
+
+print("")
+print(line01)
+print(line02)
+print(line03)
+print(line02)
+print(line01)
+
+myname = "Kita"
+if myname == "Jaylen":
+    print("My name is Jaylen")
+else:
+    print("My name is Kita")
+
