3dbd3c4a4b891d19e115a475cac947765aa7e309
nrturner1980 <nrturner99@gmail.com>
2025-04-11 00:54:11 +0000

    My Name

---
+++ meaning.py
@@ -0,0 +1,22 @@
+meaning = 42
+print('')
+
+# if meaning > 10:
+#     print('Right on!')
+# else:
+#     print('Not today')
+
+# Ternary Operator
+print('Right on!') if meaning > 10 else print('Not today')
+
+# leatn hoe to use and and or operators
+
+x = 10
+y = 20 
+z = 30
+
+if  x > 5 and y > 10:
+    print("Both are True")
+else:
+    print("Atleast 1 is not True")
+
